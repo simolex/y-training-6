34b74985fac493a82d1d3d8f736a4aa7858b1953
simolex <simolex@yandex.ru>
2024-11-13 20:51:26 +0300

    done task 3C.v2(with deque)

--- Stage-3/task-C/solve.v2.js
+++ Stage-3/task-C/solve.v2.js
@@ -1,15 +1,92 @@
+class Deque {
+    deque = [];
+    constructor() {
+        this.clear();
+    }
+
+    _getPtr(ptr) {
+        return (this.limit + ((ptr % this.limit) % this.limit)) % this.limit;
+    }
+    _checkSizeDeque() {
+        if (this.size() >= this.limit) {
+            let pos = 0;
+            const newDeque = this.deque;
+            this.deque = [];
+            for (let i = this.ptrBack; i < this.ptrFront; i++) {
+                this.deque[pos++] = newDeque[this._getPtr(i)];
+            }
+            this.ptrBack = 0;
+            this.ptrFront = pos;
+            this.limit *= 2;
+        }
+    }
+
+    push_front(n) {
+        this._checkSizeDeque();
+        this.deque[this._getPtr(this.ptrFront++)] = n;
+    }
+
+    push_back(n) {
+        this._checkSizeDeque();
+        this.deque[this._getPtr(--this.ptrBack)] = n;
+    }
+
+    pop_front() {
+        if (this.size() > 0) {
+            return this.deque[this._getPtr(--this.ptrFront)];
+        }
+        return null;
+    }
+
+    pop_back() {
+        if (this.size() > 0) {
+            return this.deque[this._getPtr(this.ptrBack++)];
+        }
+        return null;
+    }
+
+    peek_front() {
+        if (this.size() > 0) {
+            return this.deque[this._getPtr(this.ptrFront - 1)];
+        }
+        return null;
+    }
+
+    peek_back() {
+        if (this.size() > 0) {
+            return this.deque[this._getPtr(this.ptrBack)];
+        }
+        return "error";
+    }
+
+    size() {
+        return this.ptrFront - this.ptrBack;
+    }
+
+    clear() {
+        this.deque.length = 0;
+        this.ptrBack = 0;
+        this.ptrFront = 0;
+        this.limit = 2;
+    }
+}
 function minimumInRange(n, k, nums) {
     const minRange = [];
-    const heap = new MinMultiHeap();
+    const deque = new Deque();
 
-    for (let i = 0; i < k; i++) {
-        heap.add(nums[i]);
-    }
-    minRange.push(heap.viewMin().value);
+    for (let i = 0; i < n; i++) {
+        if (i >= k && deque.peek_front() === nums[i - k]) {
+            deque.pop_front();
+        }
 
-    for (let i = k; i < n; i++) {
-        heap.replace(nums[i - k], nums[i]);
-        minRange.push(heap.viewMin().value);
+        while (deque.size() > 0 && deque.peek_back() > nums[i]) {
+            deque.pop_back();
+        }
+        deque.push_back(nums[i]);
+
+        if (i >= k - 1) {
+            minRange.push(deque.peek_front());
+        }
     }
 
     return minRange;
@@ -18,7 +95,7 @@ function minimumInRange(n, k, nums) {
 const _readline = require("readline");
 
 const _reader = _readline.createInterface({
-    input: process.stdin,
+    input: process.stdin
 });
 
 const _inputLines = [];
--- Stage-3/task-J/index.js
+++ Stage-3/task-J/index.js
@@ -1,84 +1,95 @@
-class Queue {
+class Deque {
+    deque = [];
     constructor() {
-        this.queue = [];
-        this.frontPtr = 0;
+        this.clear();
+    }
+
+    _getPtr(ptr) {
+        return (this.limit + ((ptr % this.limit) % this.limit)) % this.limit;
+    }
+    _checkSizeDeque() {
+        if (this.size() >= this.limit) {
+            let pos = 0;
+            const newDeque = this.deque;
+            this.deque = [];
+            for (let i = this.ptrBack; i < this.ptrFront; i++) {
+                this.deque[pos++] = newDeque[this._getPtr(i)];
+            }
+            this.ptrBack = 0;
+            this.ptrFront = pos;
+            this.limit *= 2;
+        }
+    }
+
+    push_front(n) {
+        this._checkSizeDeque();
+        this.deque[this._getPtr(this.ptrFront++)] = n;
     }
 
     push_back(n) {
-        this.queue.push(n);
+        this._checkSizeDeque();
+        this.deque[this._getPtr(--this.ptrBack)] = n;
     }
 
     pop_front() {
         if (this.size() > 0) {
-            return this.queue[this.frontPtr++];
+            return this.deque[this._getPtr(--this.ptrFront)];
         }
+        return null;
+    }
+
+    pop_back() {
+        if (this.size() > 0) {
+            return this.deque[this._getPtr(this.ptrBack++)];
+        }
+        return null;
     }
 
     peek_front() {
         if (this.size() > 0) {
-            return this.queue[this.frontPtr];
+            return this.deque[this._getPtr(this.ptrFront - 1)];
         }
+        return null;
+    }
+
+    peek_back() {
+        if (this.size() > 0) {
+            return this.deque[this._getPtr(this.ptrBack)];
+        }
+        return "error";
     }
 
     size() {
-        return this.queue.length - this.frontPtr;
+        return this.ptrFront - this.ptrBack;
+    }
+
+    clear() {
+        this.deque.length = 0;
+        this.ptrBack = 0;
+        this.ptrFront = 0;
+        this.limit = 2;
     }
 }
+function chairsBed(n, H, heights, widths) {
+    const minRange = [];
+    const deque = new Deque();
 
-const getMainRoads = (a, b) => {
-    const decode = { 2: "R_1_2", 6: "R_2_3", 12: "R_3_4", 4: "R_1_4", 3: "R_1_3", 8: "R_2_4" };
-    return decode[a * b];
-};
-
-const orderRules = {
-    R_1_2: [[1], [2], [3], [4]],
-    R_2_3: [[2], [3], [4], [1]],
-    R_3_4: [[3], [4], [1], [2]],
-    R_1_4: [[4], [1], [2], [3]],
-    R_1_3: [
-        [1, 3],
-        [2, 4]
-    ],
-    R_2_4: [
-        [2, 4],
-        [1, 3]
-    ]
-};
-
-function automatedStore(n, a, b, rovers) {
-    const result = Array(n);
-
-    const mainCode = getMainRoads(a, b);
-    const rules = orderRules[mainCode];
-    const roads = {
-        1: new Queue(),
-        2: new Queue(),
-        3: new Queue(),
-        4: new Queue()
-    };
-    const indexRoad = Object.keys(roads);
-
-    rovers.forEach((v, i) => v.push(i));
-    rovers.sort((a, b) => a[1] - b[1]);
-    rovers.forEach((r) => roads[r[0]].push_back(r));
-
-    let time = 1;
-    let isBusy = false;
-    let curRover;
-    while (indexRoad.reduce((sum, idxQ) => sum + roads[idxQ].size(), 0) > 0) {
-        for (let rule = 0; rule < rules.length && !isBusy; rule++) {
-            for (let i = 0; i < rules[rule].length; i++) {
-                if ((curRover = roads[rules[rule][i]].peek_front()) && curRover[1] <= time) {
-                    isBusy = true;
-                    roads[rules[rule][i]].pop_front();
-                    result[curRover[2]] = time;
-                }
-            }
+    for (let i = 0; i < n; i++) {
+        if (i >= k && deque.peek_front() === nums[i - k]) {
+            deque.pop_front();
         }
-        isBusy = false;
-        time++;
+
+        while (deque.size() > 0 && deque.peek_back() > nums[i]) {
+            deque.pop_back();
+        }
+        deque.push_back(nums[i]);
+
+        if (i >= k - 1) {
+            minRange.push(deque.peek_front());
+        }
     }
-    return result;
+
+    return minRange;
 }
 
 const _readline = require("readline");
@@ -97,17 +108,12 @@ _reader.on("line", (line) => {
 process.stdin.on("end", solve);
 
 function solve() {
-    const n = readInt();
-    const [a, b] = readArray();
+    const [n, H] = readArray();
+    const heights = readArray();
+    const widths = readArray();
 
-    const rovers = [];
-    for (let i = 0; i < n; i++) {
-        rovers.push(readArray());
-    }
-
-    const result = automatedStore(n, a, b, rovers);
-
-    console.log(result.join("\n"));
+    const result = chairsBed(n, H, heights, widths);
+    console.log(result);
 }
 
 function readAllString() {
@@ -117,9 +123,9 @@ function readAllString() {
 }
 
 function readString() {
-    var str = _inputLines[_curLine] || "";
+    var str = _inputLines[_curLine].trim(" ");
     _curLine++;
-    return str.trim(" ");
+    return str;
 }
 
 function readInt() {
@@ -137,6 +143,7 @@ function readArray() {
     var arr = _inputLines[_curLine]
         .trim(" ")
         .split(" ")
+        .filter((str) => str && str.length > 0)
         .map((num) => Number(num));
     _curLine++;
     return arr;
@@ -169,4 +176,4 @@ function readEdges(n) {
     return grid;
 }
 
-module.exports = automatedStore;
+module.exports = chairsBed;
