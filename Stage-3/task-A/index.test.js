ab5693d4d1fcffe445cbb7b212ae5612ce7ffd2c
simolex <simolex@yandex.ru>
2024-11-14 16:39:53 +0300

    add description and tests for problems

--- Stage-3/task-A/index.test.js
+++ Stage-3/task-A/index.test.js
@@ -1,20 +1,20 @@
 const solution = require(".");
 
-describe("I. Правильная скобочная последовательность", () => {
+describe("Правильная скобочная последовательность", () => {
     test("test-1", () => {
         const result = solution("()[]");
         expect(result).toBe("yes");
     });
     test("test-2", () => {
         const result = solution("([)]");
-        expect(result).toEqual("no");
+        expect(result).toBe("no");
     });
     test("test-3", () => {
         const result = solution("([");
-        expect(result).toEqual("no");
+        expect(result).toBe("no");
     });
     test("test-4", () => {
         const result = solution("())(()");
-        expect(result).toEqual("no");
+        expect(result).toBe("no");
     });
 });
